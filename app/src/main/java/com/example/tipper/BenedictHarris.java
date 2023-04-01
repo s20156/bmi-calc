f1592d13b44a40d6fc5bdd892a1b69470a4a1e1b
Mateusz Grube <s20156@pjwstk.edu.pl>
2023-04-01 23:42:28 +0200

    Added things from ex2

--- app/src/main/java/com/example/tipper/BenedictHarris.java
+++ app/src/main/java/com/example/tipper/BenedictHarris.java
@@ -1,2 +1,156 @@
-package com.example.tipper;public class BenedictHarris {
+package com.example.tipper;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BenedictHarris extends AppCompatActivity {
+    private double weight = 0.0;
+    private double height = 0.15;
+    private int age = 0;
+    private boolean isMale = false;
+
+    private TextView heightTextView;
+    private TextView weightTextView;
+    private TextView ageTextView;
+    private TextView totalTextView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.benedict_harris_layout);
+
+        Switch genderSwitch = findViewById(R.id.genderSwitch);
+        weightTextView = (TextView) findViewById(R.id.weightTextView);
+        heightTextView = (TextView) findViewById(R.id.heightTextView);
+        ageTextView = (TextView) findViewById(R.id.ageTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        totalTextView.setText(String.format(String.valueOf(0)));
+
+        EditText weightEditText =
+                (EditText) findViewById(R.id.weightEditText);
+        weightEditText.addTextChangedListener(weightEditTextWatcher);
+        EditText heightEditText =
+                (EditText) findViewById(R.id.heightEditText);
+        heightEditText.addTextChangedListener(heightEditTextWatcher);
+
+        EditText ageEditText =
+                (EditText) findViewById(R.id.ageEditText);
+        ageEditText.addTextChangedListener(ageEditTextWatcher);
+
+        genderSwitch.setOnCheckedChangeListener(switchChangeListener);
+    }
+
+    // listeners object for TextViews weight and height text-changed events
+    private final TextWatcher weightEditTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                weight = Double.parseDouble(s.toString());
+                weightTextView.setText(String.format(String.valueOf(weight)));
+
+            } catch (NumberFormatException e) {
+                weightTextView.setText("");
+                weight = 0.0;
+
+            }
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) {
+        }
+    };
+    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                height = Double.parseDouble(s.toString());
+                heightTextView.setText(String.format(String.valueOf(height)));
+            } catch (NumberFormatException e) {
+                heightTextView.setText("");
+                height = 0.0;
+            }
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+    private final TextWatcher ageEditTextWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                age = Integer.parseInt(s.toString());
+                ageTextView.setText(String.format(String.valueOf(age)));
+            } catch (NumberFormatException e) {
+                age = 0;
+            }
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private final CompoundButton.OnCheckedChangeListener switchChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            try {
+                isMale = isChecked;
+            } catch (Exception e) {
+                isMale = false;
+            }
+            calculate();
+        }
+    };
+
+    public void calculate() {
+        double total = 0.00;
+
+        if (isMale) {
+            total = 66.47 + (13.7 * weight) + (5.0 * height) - (6.76 * age);
+        } else {
+            total = 655.1 + (9.567 * weight) + (1.95 * height) - (4.68 * age);
+        }
+
+        totalTextView.setText(String.format(String.valueOf(total)));
+    }
 }
--- app/src/main/java/com/example/tipper/MainActivity.java
+++ app/src/main/java/com/example/tipper/MainActivity.java
@@ -2,110 +2,66 @@ package com.example.tipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable; // for EditText event handling
 import android.text.TextWatcher; // EditText listener
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText; // for bill amount input
 import android.widget.SeekBar; // for changing the tip percentage
 import android.widget.SeekBar.OnSeekBarChangeListener; // SeekBar listener
 import android.widget.TextView; // for displaying text
 
 import java.text.NumberFormat; // for currency formatting
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // currency and percent formatter objects
+    private Button bmiButton;
+    private Button recipeButton;
+    private Button bhButton;
 
-    private double weight = 65.0;
-    private double height = 165.0;
-    private TextView heightTextView;
-    private TextView weightTextView;
-    private TextView bmiTextView;
-
-    // called when the activity is first created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // call superclass onCreate
-        setContentView(R.layout.activity_main); // inflate the GUI
-
-        // get references to programmatically manipulated TextViews
-        bmiTextView = (TextView) findViewById(R.id.bmiTextView);
-        bmiTextView.setText(R.string.total);
-
-        weightTextView = (TextView) findViewById(R.id.weightTextView);
-        heightTextView = (TextView) findViewById(R.id.heightTextView);
-        // set weightEditText's TextWatcher
-        EditText weightEditText =
-                (EditText) findViewById(R.id.weightEditText);
-        weightEditText.addTextChangedListener(weightEditTextWatcher);
-
-        // set heightEditText's TextWatcher
-        EditText heightEditText =
-                (EditText) findViewById(R.id.heightEditText);
-        heightEditText.addTextChangedListener(heightEditTextWatcher);
-    }
-
-    // calculate and display BMI
-    private void calculate() {
-        // calculate the BMI
-        double bmi = weight / (height * height);
-
-        // display tip and total formatted as currency
-        bmiTextView.setText(String.format(String.valueOf(bmi)));
-    }
-
-    // listener object for the EditText's text-changed events
-    private final TextWatcher weightEditTextWatcher = new TextWatcher() {
-        // called when the user modifies the bill amount
-        @Override
-        public void onTextChanged(CharSequence s, int start,
-                                  int before, int count) {
-
-            try { // get weight and display value
-                weight = Double.parseDouble(s.toString());
-                weightTextView.setText(String.format(String.valueOf(weight)));
+        setContentView(R.layout.main_layout);
+        bmiButton = (Button) findViewById(R.id.bmiButton);
+        bhButton = (Button) findViewById(R.id.benedictHarris);
+        recipeButton = (Button) findViewById(R.id.recipe);
+        bmiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBmiActivity();
             }
-            catch (NumberFormatException e) { // if s is empty or non-numeric
-                weightTextView.setText("");
-                weight = 0.0;
+        });
+
+        bhButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBHActivity();
             }
-
-            calculate(); // update the bmi TextView
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) { }
-
-        @Override
-        public void beforeTextChanged(
-                CharSequence s, int start, int count, int after) { }
+        });
+        recipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRecipeActivity();
+            }
+        });
     };
-    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
 
-        @Override
-        public void onTextChanged(CharSequence s, int start,
-                                  int before, int count) {
-
-            try {
-                height = Double.parseDouble(s.toString()) / 100.0;
-                heightTextView.setText(String.format(String.valueOf(height)));
-            } catch (NumberFormatException e) { // if s is empty or non-numeric
-                heightTextView.setText("");
-                height = 0.0;
-            }
-
-            calculate(); // update the tip and total TextViews
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-        }
-
-        @Override
-        public void beforeTextChanged(
-                CharSequence s, int start, int count, int after) {
-        }
-    };
+    public void openBmiActivity() {
+        Intent intent = new Intent(this, BMI.class);
+        startActivity(intent);
+    }
+    public void openBHActivity() {
+        Intent intent = new Intent(this, BenedictHarris.class);
+        startActivity(intent);
+    }
+    public void openRecipeActivity() {
+        Intent intent = new Intent(this, Recipe.class);
+        startActivity(intent);
+    }
 }
 
 
